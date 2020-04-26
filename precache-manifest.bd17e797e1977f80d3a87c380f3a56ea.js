302d55d9c044ac1bff7cb40edb325870da8efe6f
Fabio <fabio@chiarani.it>
2020-04-26 15:11:58 +0200

    Updates

---
+++ precache-manifest.bd17e797e1977f80d3a87c380f3a56ea.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c5b156ba4ea28ee12f3d7371d34454c",
+    "url": "/algo-sorting/index.html"
+  },
+  {
+    "revision": "858a274e3af9af447aed",
+    "url": "/algo-sorting/static/css/2.1950603c.chunk.css"
+  },
+  {
+    "revision": "e35538d8f9cfcc34bfde",
+    "url": "/algo-sorting/static/css/main.dd49c646.chunk.css"
+  },
+  {
+    "revision": "858a274e3af9af447aed",
+    "url": "/algo-sorting/static/js/2.1121ec04.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/algo-sorting/static/js/2.1121ec04.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e35538d8f9cfcc34bfde",
+    "url": "/algo-sorting/static/js/main.b21b2e7f.chunk.js"
+  },
+  {
+    "revision": "d3229a64f8c071cb4af6",
+    "url": "/algo-sorting/static/js/runtime-main.9efd9a1e.js"
+  }
+]);
